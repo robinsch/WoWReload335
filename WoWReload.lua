6e731e8be6e32a939b2d74d33993127a5360f252
robinsch <hello@robinsch.com>
2024-02-21 08:25:24 +0100

    Added DropDown menu to clear WDB caches

--- WoWReload.lua
+++ WoWReload.lua
@@ -1,30 +1,216 @@
 local addonName = ...
 
 WoWReloadPanelOptions = {
-	watchADT = { text = ".adt" },
-	watchWDT = { text = ".wdt" },
-	watchWDL = { text = ".wdl" },
-	watchTEX = { text = ".tex" },
-	watchBLP = { text = ".blp" },
-	watchM2 =  { text = ".m2"  },
-	watchWMO = { text = ".wmo" },
-	watchDBC = { text = ".dbc" },
-	watchLUA = { text = ".lua" },
-	watchXML = { text = ".xml" },
-	watchTOC = { text = ".toc" },
+    watchADT = { text = ".adt" },
+    watchWDT = { text = ".wdt" },
+    watchWDL = { text = ".wdl" },
+    watchTEX = { text = ".tex" },
+    watchBLP = { text = ".blp" },
+    watchM2 =  { text = ".m2"  },
+    watchWMO = { text = ".wmo" },
+    watchDBC = { text = ".dbc" },
+    watchLUA = { text = ".lua" },
+    watchXML = { text = ".xml" },
+    watchTOC = { text = ".toc" },
 }
 
 function WoWReload_OnLoad(self)
-	self:RegisterEvent("ADDON_LOADED")
-	self.name = addonName;
-	self.options = WoWReloadPanelOptions;
+    self:RegisterEvent("ADDON_LOADED")
+    self:RegisterEvent("PLAYER_ENTERING_WORLD")
+    self.name = addonName;
+    self.options = WoWReloadPanelOptions;
 end
 
 function WoWReload_OnEvent(self, event, ...)
-	if event == "ADDON_LOADED" then
-		local addon = ...
-		if addon == addonName then
-			InterfaceOptionsPanel_OnLoad(self);
-		end
-	end
+    if event == "ADDON_LOADED" then
+        local addon = ...
+        if addon == addonName then
+            InterfaceOptionsPanel_OnLoad(self);
+        end
+    end
+end
+
+function WoWReloadWDBDisplay_OnClick(self)
+    WoWReloadWDBDisplay:SetValue(self.value);
+end
+
+function WoWReloadWDBDisplay_Initialize()
+    local selectedValue = UIDropDownMenu_GetSelectedValue(WoWReloadWDBDisplay);
+    local info = UIDropDownMenu_CreateInfo();
+    info.tooltipOnButton = 1;
+
+    info.text = "Creature";
+    info.func = WoWReloadWDBDisplay_OnClick;
+    info.value = "1";
+    if ( info.value == selectedValue ) then
+        info.checked = 1;
+    else
+        info.checked = nil;
+    end
+    UIDropDownMenu_AddButton(info);
+
+    info.text = "GameObject";
+    info.func = WoWReloadWDBDisplay_OnClick;
+    info.value = "2";
+    if ( info.value == selectedValue ) then
+        info.checked = 1;
+    else
+        info.checked = nil;
+    end
+    UIDropDownMenu_AddButton(info);
+
+    info.text = "Item";
+    info.func = WoWReloadWDBDisplay_OnClick;
+    info.value = "3";
+    if ( info.value == selectedValue ) then
+        info.checked = 1;
+    else
+        info.checked = nil;
+    end
+    UIDropDownMenu_AddButton(info);
+
+    info.text = "ItemName";
+    info.func = WoWReloadWDBDisplay_OnClick;
+    info.value = "4";
+    if ( info.value == selectedValue ) then
+        info.checked = 1;
+    else
+        info.checked = nil;
+    end
+
+    info.text = "ItemText";
+    info.func = WoWReloadWDBDisplay_OnClick;
+    info.value = "5";
+    if ( info.value == selectedValue ) then
+        info.checked = 1;
+    else
+        info.checked = nil;
+    end
+
+    info.text = "NPC";
+    info.func = WoWReloadWDBDisplay_OnClick;
+    info.value = "6";
+    if ( info.value == selectedValue ) then
+        info.checked = 1;
+    else
+        info.checked = nil;
+    end
+
+    info.text = "PageText";
+    info.func = WoWReloadWDBDisplay_OnClick;
+    info.value = "7";
+    if ( info.value == selectedValue ) then
+        info.checked = 1;
+    else
+        info.checked = nil;
+    end
+    UIDropDownMenu_AddButton(info);
+
+    info.text = "Quest";
+    info.func = WoWReloadWDBDisplay_OnClick;
+    info.value = "8";
+    if ( info.value == selectedValue ) then
+        info.checked = 1;
+    else
+        info.checked = nil;
+    end
+    UIDropDownMenu_AddButton(info);
+
+    info.text = "WoW";
+    info.func = WoWReloadWDBDisplay_OnClick;
+    info.value = "9";
+    if ( info.value == selectedValue ) then
+        info.checked = 1;
+    else
+        info.checked = nil;
+    end
+        UIDropDownMenu_AddButton(info);
+
+    info.text = "Name";
+    info.func = WoWReloadWDBDisplay_OnClick;
+    info.value = "10";
+    if ( info.value == selectedValue ) then
+        info.checked = 1;
+    else
+        info.checked = nil;
+    end
+        UIDropDownMenu_AddButton(info);
+
+    info.text = "PetName";
+    info.func = WoWReloadWDBDisplay_OnClick;
+    info.value = "11";
+    if ( info.value == selectedValue ) then
+        info.checked = 1;
+    else
+        info.checked = nil;
+    end
+        UIDropDownMenu_AddButton(info);
+
+    info.text = "Guild";
+    info.func = WoWReloadWDBDisplay_OnClick;
+    info.value = "12";
+    if ( info.value == selectedValue ) then
+        info.checked = 1;
+    else
+        info.checked = nil;
+    end
+
+    info.text = "Petition";
+    info.func = WoWReloadWDBDisplay_OnClick;
+    info.value = "13";
+    if ( info.value == selectedValue ) then
+        info.checked = 1;
+    else
+        info.checked = nil;
+    end
+        UIDropDownMenu_AddButton(info);
+
+    info.text = "ArenaTeam";
+    info.func = WoWReloadWDBDisplay_OnClick;
+    info.value = "14";
+    if ( info.value == selectedValue ) then
+        info.checked = 1;
+    else
+        info.checked = nil;
+    end
+        UIDropDownMenu_AddButton(info);
+
+    info.text = "Dance";
+    info.func = WoWReloadWDBDisplay_OnClick;
+    info.value = "15";
+    if ( info.value == selectedValue ) then
+        info.checked = 1;
+    else
+        info.checked = nil;
+    end
+    UIDropDownMenu_AddButton(info);
+end
+
+function WoWReloadWDBDisplay_OnEvent(self, event, ...)
+    if ( event == "PLAYER_ENTERING_WORLD" ) then
+        self.defaultValue = 1;
+        self.value = 1;
+        self.oldValue = 1;
+
+        UIDropDownMenu_SetWidth(self, 90);
+        UIDropDownMenu_Initialize(self, WoWReloadWDBDisplay_Initialize);
+        UIDropDownMenu_SetSelectedValue(self, value);
+
+        self.SetValue = 
+            function (self, value)
+                self.value = value;
+                UIDropDownMenu_SetSelectedValue(self, value);
+            end
+        self.GetValue =
+            function (self)
+                return UIDropDownMenu_GetSelectedValue(self);
+            end
+        self.RefreshValue =
+            function (self)
+                UIDropDownMenu_Initialize(self, WoWReloadWDBDisplay_Initialize);
+                UIDropDownMenu_SetSelectedValue(self, self.value);
+            end
+            
+        self:UnregisterEvent(event);
+    end
 end
